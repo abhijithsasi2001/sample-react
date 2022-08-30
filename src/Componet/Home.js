ca6ce7c94238254138e69995e0852d6c394a1d60
abhijithsasi2001 <112365953+abhijithsasi2001@users.noreply.github.com>
2022-08-30 11:25:53 +0530

    Update Home.js

--- src/Componet/Home.js
+++ src/Componet/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Home() {
   return (
     <div className="welcome">
-        <img src="banner.webp"></img>
+        <img src="banner.webp" alt="images"></img>
         <h1 className="home">WELCOME</h1>
     </div>
   )
